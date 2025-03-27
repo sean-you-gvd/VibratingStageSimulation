530865609c005bdfad8b4006fcad7a82c9069135
sean-you-gvd <syou@gvdcorp.com>
2025-03-27 17:33:36 -0400

    updated with sim params file

---
+++ SimParams.py
@@ -0,0 +1,29 @@
+#Particle configuration
+nx=3
+nz=3
+ny=4
+height=10
+visual=True
+random_particle=True
+friction=1
+adhesion=25
+
+#Time configuration
+motor_on_time=5
+endtime = 1000.0
+time_step=1e-3
+out_step  = 1.0 / 20
+
+#Motor configuration
+sine=False
+
+A1=0.05
+f1=30
+
+A2=.20
+f2=1
+nlp=12
+
+
+#outpath='blender-longtime-ny4-secondary'
+outpath='long-time-secondary'
--- VibratingStage.py
+++ VibratingStage.py
@@ -12,7 +12,7 @@
 
 
 import pychrono.core as chrono
-#import pychrono.irrlicht as chronoirr
+import pychrono.irrlicht as chronoirr
 import pychrono.postprocess as postprocess
 
 import pathlib
@@ -20,8 +20,20 @@ import pathlib
 import errno
 import os
 import importlib.util
+import random
 
 import os
+import numpy as np
+import matplotlib.pyplot as plt
+
+import os
+import sys
+import shutil
+
+script_dir = os.path.dirname(os.path.abspath(__file__))
+sys.path.append(script_dir)
+
+from SimParams import*
 
 print ("Vibrating Stage Simulation")
 
@@ -30,24 +42,37 @@ print ("Vibrating Stage Simulation")
 # If running from a different directory, you must change the path to the data directory with: 
 #chrono.SetChronoDataPath('relative/path/to/data/directory/')
 
-bowlpath=pathlib.Path().resolve()/'bowls'/'spike bowl.obj'
-nx=7
-nz=7
-ny=3
+#bowlpath=pathlib.Path().resolve()/'bowls'/'revised spike bowl.obj'
+
+#bowlpath=pathlib.Path().resolve()/'bowls'/'hemisphere bowl.obj'
+
+bowlpath=pathlib.Path().resolve()/'bowls'/'bowl 3_6.obj'
 
 
 
+
+
+class ChFunctionMyFun (chrono.ChFunction):
+    def __init__(self):
+         chrono.ChFunction.__init__(self)
+    def GetVal(self,x):
+        return A1*np.sin(2*np.pi*f1*x)+A2*(np.sin(2*np.pi*f2*x))**nlp
+
+if sine:
+    mfun=chrono.ChFunctionSine(A1,f1,0)
+else:
+    mfun=ChFunctionMyFun()
 
 def AddFallingItems(sys):
     # Shared contact materials for falling objects
     bodylist=[]
     mat = chrono.ChContactMaterialSMC()
-    mat.SetFriction(1e-3)
-    mat.SetAdhesion(1e-3)
+    mat.SetFriction(friction)
+    mat.SetAdhesion(adhesion)
     # Create falling rigid bodies (spheres and boxes etc.)
-    for ix in range(-7, 7):
-        for iz in range(-7, 7):
-            for iy in range(0,3):
+    for ix in range(-1*nx, nx):
+        for iz in range(-1*nz, nz):
+            for iy in range(height,height+ny):
                 # add spheres
                 mass = .1
                 #radius = .05
@@ -58,7 +83,10 @@ def AddFallingItems(sys):
                 body.SetMass(mass)
                 #body.SetPos(chrono.ChVector3d(.1 * ix + 0.1, -2,.1 * iz))
                 #body.SetPos(chrono.ChVector3d(3 * ix + 0.1, -2,3 * iz))
-                body.SetPos(chrono.ChVector3d(4 * ix + 0.1, 2*iy-2,4 * iz))
+                if random_particle:
+                    body.SetPos(chrono.ChVector3d(4 * ix + (.1*random.random()-1), 2*iy-2,4 * iz+(.1*random.random()-1)))
+                else:
+                    body.SetPos(chrono.ChVector3d(4 * ix + 0.1, 2*iy-2,4 * iz))                    
                 body_ct_shape = chrono.ChCollisionShapeSphere(mat, radius)
                 body.AddCollisionShape(body_ct_shape)
                 body.EnableCollision(True)
@@ -69,6 +97,7 @@ def AddFallingItems(sys):
 
                 sys.AddBody(body)
                 bodylist.append(body)
+    return bodylist
 
 def AddContainerWall(body, mat, size, pos, visible=True):
     body_ct_shape = chrono.ChCollisionShapeBox(mat, size.x, size.y, size.z)
@@ -113,6 +142,8 @@ def AddContainer(sys):
 
     # Contact material for mixer body
     mixer_mat = chrono.ChContactMaterialSMC()
+    #mixer_mat.SetAdhesion(1000000000000)
+    #mixer_mat.SetFriction(100)
 #    exported_items = chrono.ImportSolidWorksSystem(chrono.GetChronoDataFile('spline bowl.STEP'))
 
 #    exported_items = chrono.ChBodyEasyMesh('hemisphere bowl.obj',  # x,y,z size
@@ -145,28 +176,35 @@ def AddContainer(sys):
 
     # A motor between the two
     #my_motor = chrono.ChLinkMotorRotationSpeed()
-    my_motor=chrono.ChLinkMotorLinearSpeed()
+#    my_motor=chrono.ChLinkMotorLinearSpeed()
+    my_motor=chrono.ChLinkMotorLinearPosition()
     my_motor.Initialize(
                         exported_items,
                         #rotatingBody,
                         fixedBody,
                         chrono.ChFramed(chrono.ChVector3d(0, 0, 0), 
                                         chrono.QuatFromAngleAxis(chrono.CH_PI_2, chrono.VECT_X)))
-    mfun=chrono.ChFunctionSine(10,10,2)
+    deadfun=chrono.ChFunctionSine(0,0,0)
+    mfun=chrono.ChFunctionSine(.05,30,2)
 
     #mfun = chrono.ChFunctionConst(chrono.CH_PI / 2.0)  # speed w=90°/s
-    my_motor.SetSpeedFunction(mfun)
-
+#    my_motor.SetSpeedFunction(mfun)
+    my_motor.SetMotionFunction(deadfun)
     sys.AddLink(my_motor)
 
-    return rotatingBody
+    return rotatingBody,my_motor
 
 # ---------------------------------------------------------------------
 #
 #  Create the simulation sys and add items
 #
 
+#sys = chrono.ChSystemSMC()
 sys = chrono.ChSystemSMC()
+
+
+#print(sys.GetSolver())
+#sys.SetSolver()
 #sys = chrono.ChSystemNSC()
 
 
@@ -176,54 +214,83 @@ sys.SetCollisionSystemType(chrono.ChCollisionSystem.Type_BULLET)
 
 
 # Simulation and rendering time-step
-time_step = 1e-4
-out_step  = 1.0 / 20
+#time_step = 1e-4
+
 
 # Add fixed and moving bodies
-mixer = AddContainer(sys)
-AddFallingItems(sys)
+a,motor = AddContainer(sys)
+bodies=AddFallingItems(sys)
 
 # Create the Irrlicht visualization
-#vis = chronoirr.ChVisualSystemIrrlicht()
-#vis.AttachSystem(sys)
-#vis.SetWindowSize(1024,768)
-#vis.SetWindowTitle('Collisions between objects')
-#vis.Initialize()
-#vis.AddLogo(chrono.GetChronoDataFile('logo_pychrono_alpha.png'))
-#vis.AddSkyBox()
-#vis.AddCamera(chrono.ChVector3d(0, 18, -20))
-#vis.AddCamera(chrono.ChVector3d(0, 80, -40))
-#vis.AddTypicalLights()
-#vis.AddLight(chrono.ChVector3d(400,2000,0),2000,chrono.ChColor(1,1,1))
-#vis.AddLight(chrono.ChVector3d(0,-2000,0),2000,chrono.ChColor(1,1,1))
+if visual==True:
+    vis = chronoirr.ChVisualSystemIrrlicht()
+    vis.AttachSystem(sys)
+    vis.SetWindowSize(1024,768)
+    vis.SetWindowTitle('Collisions between objects')
+    vis.Initialize()
+    #vis.AddLogo(chrono.GetChronoDataFile('logo_pychrono_alpha.png'))
+    vis.AddSkyBox()
+    vis.AddCamera(chrono.ChVector3d(0, 18, -20))
+    vis.AddCamera(chrono.ChVector3d(0, 80, -40))
+    #vis.AddTypicalLights()
+    vis.AddLight(chrono.ChVector3d(400,2000,0),2000,chrono.ChColor(1,1,1))
+    vis.AddLight(chrono.ChVector3d(0,-2000,0),2000,chrono.ChColor(1,1,1))
+    vis.Run()
 #pov_exporter = postprocess.ChPovRay(sys)
 #pov_exporter.SetTemplateFile(chrono.GetChronoDataFile("POVRay_chrono_template.pov"))
 #pov_exporter.SetBasePath("povray1")
 
 pov_exporter = postprocess.ChBlender(sys)
-#pov_exporter.SetTemplateFile(chrono.GetChronoDataFile("POVRay_chrono_template.pov"))
-pov_exporter.SetBasePath("blender")
+pov_exporter.SetBasePath(outpath)
 
 pov_exporter.AddAll()
 pov_exporter.ExportScript()
 
 
 
-# Simulation loop
+# Simulation loop - Initalize Time
 time     = 0.0
 out_time = 0.0
-endtime = 300.0
-while time<endtime:
+
+motor_off=True
+data=[]
+while vis.Run():
     sys.DoStepDynamics(time_step)
     time = sys.GetChTime()
+    if (time>motor_on_time) and motor_off: 
+        motor_off=False
+        motor.SetMotionFunction(mfun)
+        print('Motor Turning On!')
     if (time >= out_time and time < endtime):
-#        vis.BeginScene() 
- #       vis.Render()
-#        vis.EndScene()
+        if visual==True:
+            vis.BeginScene() 
+            vis.Render()
+            vis.EndScene()
         pov_exporter.ExportData()
         print ('time=', sys.GetChTime() )
         out_time += out_step
+        data.append(bodies[0].GetCenter())
+    if time>endtime: vis.Quit()
    
+
+datax=list(map(lambda ele: data[ele].x, [n for n in range(0,len(data))]))
+datay=list(map(lambda ele: data[ele].y, [n for n in range(0,len(data))]))
+dataz=list(map(lambda ele: data[ele].z, [n for n in range(0,len(data))]))
+
+datat=np.arange(0,len(data))*out_step
+fig,ax=plt.subplots(nrows=3,ncols=1)
+
+ax[0].plot(datat,datax)
+ax[1].plot(datat,datay)
+ax[2].plot(datat,dataz)
+fig.savefig(os.path.join(outpath,'test.png'))
+
+fig.show()
+
+shutil.copy(os.path.join(script_dir,'SimParams.py'),os.path.join(outpath,'SimParams.py'))
+
+#plt.plot(x)
+
         # print out contact force and torque
         # frc = mixer.GetAppliedForce()
         # trq = mixer.GetAppliedTorque()
